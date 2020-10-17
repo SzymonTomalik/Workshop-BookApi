56ef3612f8f658f9d34859614e16a7dbb790d6f5
SzymonTomalik <stomalik@gmail.com>
2020-10-17 15:12:43 +0200

    encodingFilter

--- src/main/java/pl/coderslab/AppInitializer.java
+++ src/main/java/pl/coderslab/AppInitializer.java
@@ -1,8 +1,12 @@
 package pl.coderslab;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -19,5 +23,20 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new CharacterEncodingFilter()};
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        FilterRegistration.Dynamic filter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        filter.setInitParameter("encoding", "UTF-8");
+        filter.setInitParameter("forceEncoding", "true");
+        filter.addMappingForUrlPatterns(null, true, "/*");
+    }
+
 
 }

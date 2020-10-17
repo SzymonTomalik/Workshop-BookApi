e4b0d66e0cd6d9caf31d9e97041ddc7d834d65d4
SzymonTomalik <stomalik@gmail.com>
2020-10-17 15:15:52 +0200

    fixed

--- src/main/java/pl/coderslab/domain/BookServiceInterface.java
+++ src/main/java/pl/coderslab/domain/BookServiceInterface.java
@@ -1,18 +1,19 @@
 package pl.coderslab.domain;
 
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface BookServiceInterface {
 
     List<Book> getBooks();
-    void add(Book book);
-    Optional <Book> get(Long id);
-    void delete(Long id);
-    void update(Book book);
 
+    void add(Book book);
+
+    Optional<Book> get(Long id);
+
+    void delete(Long id);
+
+    void update(Book book);
 
 
 }
